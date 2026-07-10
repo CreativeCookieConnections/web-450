b9db6e3acd85dd7e32d605faddd3610d82ac8051
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-10 18:24:04 -0500

    applied fixes to errors and also implemented unit testing

--- gms/gms-client/src/app/app.component.spec.ts
+++ gms/gms-client/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to the Garden Management System');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Gardening Management System');
   });
 });
