cf1435f58b5e91fd1f95443c62898a6dadf033ac
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-10 18:05:13 -0500

    created functions for calling the Garden APIs

--- gms/gms-client/src/app/garden/garden.service.ts
+++ gms/gms-client/src/app/garden/garden.service.ts
@@ -1,9 +1,32 @@
+import { Garden } from './garden';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GardenService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGardens() {
+    return this.http.get<Garden[]>(`${environment.apiBaseUrl}/api/gardens`);
+  }
+
+  getGarden(gardenId: number) {
+    return this.http.get<Garden>(`${environment.apiBaseUrl}/api/gardens/${gardenId}`);
+  }
+
+  addGarden(garden: Garden) {
+    return this.http.post<Garden>(`${environment.apiBaseUrl}/api/gardens`, garden);
+}
+
+  updateGarden(garden: Garden) {
+    return this.http.put<Garden>(`${environment.apiBaseUrl}/api/gardens/${garden.gardenId}`, garden);
+  }
+
+  deleteGarden(gardenId: number) {
+    return this.http.delete(`${environment.apiBaseUrl}/api/gardens/${gardenId}`);
+  }
 }
