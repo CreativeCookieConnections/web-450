63cf451e6198825ac78df9907a218f2517a176c9
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-10 18:00:47 -0500

    Added code which implements the Garden interface

--- gms/gms-client/src/app/garden/garden.ts
+++ gms/gms-client/src/app/garden/garden.ts
@@ -1,2 +1,9 @@
 export interface Garden {
+    _id: string;
+    gardenId: number;
+    name: string;
+    location: string;
+    description?: string;
+    dateCreated?: string;
+    dateModified: string;
 }
