283dea347d66b4991d36dc8e23abc08a08383c8c
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 18:18:05 -0500

    Added a DTO object for adding new plant documents to MongoDB database. Also added a DTO object for updating an existing plant document in the MongoDB database.

--- gms/gms-client/src/app/plant/plant.ts
+++ gms/gms-client/src/app/plant/plant.ts
@@ -9,3 +9,7 @@ export interface Plant {
     dateCreated?: string;
     dateModified?: string;
 }
+
+export type AddPlantDTO = Omit<Plant, '_id' | 'gardenId' | 'dateHarvested' | 'dateCreated' | 'dateModified'>;
+
+export type UpdatePlantDTO = Omit<Plant, '_id' | 'gardenId' | 'datePlanted' | 'dateHarvested' | 'dateCreated' | 'dateModified'>;
