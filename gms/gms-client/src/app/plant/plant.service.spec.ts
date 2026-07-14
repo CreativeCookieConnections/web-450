c2cee0452e208d4c90f3f600dbe934c57409bdd0
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 18:34:02 -0500

    Get plant by Id

--- gms/gms-client/src/app/plant/plant.service.spec.ts
+++ gms/gms-client/src/app/plant/plant.service.spec.ts
@@ -24,6 +24,7 @@ describe('PlantService', () => {
     httpMock.verify();
   });
 
+  // GET /api/plants
   it('should retrieve a list of plants from the API', () => {
     const mockPlants: Plant[] = [
       {_id: '1', gardenId: 1, name: 'Rose', type: 'Flower', status: 'Planted', datePlanted: '2023-01-01'},
@@ -38,5 +39,18 @@ describe('PlantService', () => {
     const req = httpMock.expectOne(`${environment.apiBaseUrl}/api/plants`);
     expect(req.request.method).toBe('GET');
     req.flush(mockPlants);
+  });
+
+  // GET /api/plants/:plantId
+  it('should retrieve a single plant by ID from the API', () => {
+    const mockPlant: Plant = {_id: '1', gardenId: 1, name: 'Rose', type: 'Flower', status: 'Planted', datePlanted: '2023-01-01'};
+
+    service.getPlants('1').subscribe(plant => {
+      expect(plant).toEqual(mockPlant);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/api/plants/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPlant);
   });
 });
