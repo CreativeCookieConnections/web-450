b257cbb35a0de804219d8a5b83f696988fcee557
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 18:26:01 -0500

    Create unite test that checks if a list of plants is returned from API

--- gms/gms-client/src/app/plant/plant.service.spec.ts
+++ gms/gms-client/src/app/plant/plant.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PlantService } from './plant.service';
+import { Plant } from './plant';
+import { environment } from '../../environments/environment';
+
+
+
 
 describe('PlantService', () => {
   let service: PlantService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlantService]
+    });
     service = TestBed.inject(PlantService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve a list of plants from the API', () => {
+    const mockPlants: Plant[] = [
+      {_id: '1', gardenId: 1, name: 'Rose', type: 'Flower', status: 'Planted', datePlanted: '2023-01-01'},
+      {_id: '2', gardenId: 1, name: 'Tulip', type: 'Flower', status: 'Planted', datePlanted: '2023-02-01'}
+    ];
+
+    service.getPlants().subscribe(plants => {
+      expect(plants.length).toBe(2);
+      expect(plants).toEqual(mockPlants);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/api/plants`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPlants);
   });
 });
