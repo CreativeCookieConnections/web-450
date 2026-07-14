f53d77a23f9a217fc3d12aec066dcb844b03d09a
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 18:56:04 -0500

    Added a new unit test to check if the PlantListComponent is created.

--- gms/gms-client/src/app/plant/plant-list/plant-list.component.spec.ts
+++ gms/gms-client/src/app/plant/plant-list/plant-list.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlantListComponent } from './plant-list.component';
+import { PlantService } from '../plant.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let plantService: PlantService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlantListComponent]
+      imports: [HttpClientTestingModule, RouterTestingModule, PlantListComponent], // Import PlantListComponent
+      providers: [PlantService] // Provide PlantService
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    plantService = TestBed.inject(PlantService);
   });
 
   it('should create', () => {
