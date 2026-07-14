0fee1dff457584c5249a06756c92338dd54a7025
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 18:13:52 -0500

    Created a TypeScript interface that resembles the structure of the Plant model from our API layer.

--- gms/gms-client/src/app/plant/plant.ts
+++ gms/gms-client/src/app/plant/plant.ts
@@ -1,2 +1,11 @@
 export interface Plant {
+    _id: string;
+    gardenId: number;
+    name: string;
+    type: string;
+    status: string;
+    datePlanted?: string;
+    dateHarvested?: string;
+    dateCreated?: string;
+    dateModified?: string;
 }
