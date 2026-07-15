9477a2a7250e7e20fa8596755700acbed7d77cd8
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 19:40:46 -0500

    Wrote the minimum implementation code for the test to pass.

--- gms/gms-client/src/app/plant/plant-list/plant-list.component.ts
+++ gms/gms-client/src/app/plant/plant-list/plant-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PlantService } from '../plant.service';
+import { Plant } from '../plant';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -8,10 +9,36 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink],
   template: `
-    <div>Plant List Component</div>
+    <div class="plant-page">
+      <h1 class ="plant-page_title">Plant List</h1>
+
+      <table *ngIf="plants && plants.length > 0" class="plant-page_table">
+        <thead class="plant-page_table-head">
+          <tr class="plant-page_table-row">
+            <th class="plant-page_table-header">PlantID</th>
+            <th class="plant-page_table-header">Name</th>
+            <th class="plant-page_table-header">Type</th>
+            <th class="plant-page_table-header">Status</th>
+            <th class="plant-page_table-header">Date Planted</th>
+          </tr>
+        </thead>
+        <tbody class="plant-page_table-body">
+          <tr *ngFor="let plant of plants" class="plant-page_table-row">
+            <td class="plant-page_table-cell">{{plant._id}}</td>
+            <td class="plant-page_table-cell">{{plant.name}}</td>
+            <td class="plant-page_table-cell">{{plant.type}}</td>
+            <td class="plant-page_table-cell">{{plant.status}}</td>
+            <td class="plant-page_table-cell">{{plant.datePlanted | date}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p *ngIf="!plants || plants.length === 0" class="plant-page_no-plants">No plants found, consider adding one...</p>
+    </div>
   `,
 })
 export class PlantListComponent {
-  constructor(private plantService: PlantService) {
-  }
+  plants: Plant[] = [];
+
+
+  constructor(private plantService: PlantService) {}
 }
