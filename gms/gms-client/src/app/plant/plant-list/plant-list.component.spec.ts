d0b19017096de1e6011e72734032d1bf598a0164
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 19:03:30 -0500

    Add a new unit test to check if the DOM correctly displays plant documents.

--- gms/gms-client/src/app/plant/plant-list/plant-list.component.spec.ts
+++ gms/gms-client/src/app/plant/plant-list/plant-list.component.spec.ts
@@ -3,7 +3,9 @@ import { PlantListComponent } from './plant-list.component';
 import { PlantService } from '../plant.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Plant } from '../plant';
+import { By } from '@angular/platform-browser';
+import it from '@angular/common/locales/extra/it';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
@@ -24,5 +26,19 @@ describe('PlantListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Check if the DOM correctly displays plant documents
+  it('should display records in the DOM', () => {
+    const mockPlants: Plant[] = [
+      {_id: '1', gardenId: 1, name: 'Rose', type: 'Flower', status: 'Planted', datePlanted: '2023-01-01'},
+      {_id: '2', gardenId: 1, name: 'Tulip', type: 'Flower', status: 'Planted', datePlanted: '2023-02-01'}
+    ];
+
+    component.plants = mockPlants;
+    fixture.detectChanges(); // Trigger change detection
+
+    const plantRows = fixture.debugElement.queryAll(By.css('.plant-page_table-body .plant-page_table-row'));
+    expect(plantRows.length).toBeGreaterThan(0); // Ensure there are rows in the DOM
   });
 });
