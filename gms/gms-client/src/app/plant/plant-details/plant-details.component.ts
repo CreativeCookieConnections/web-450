e00e962f334c7ec1fffffc89c655060774e319ee
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-14 20:25:11 -0500

    Write minimum code for the unit test to pass

--- gms/gms-client/src/app/plant/plant-details/plant-details.component.ts
+++ gms/gms-client/src/app/plant/plant-details/plant-details.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { PlantService } from '../plant.service';
+import { Plant } from '../plant';
 
 @Component({
-  selector: 'app-plant-details',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      plant-details works!
-    </p>
-  `,
-  styles: ``
+	selector: 'app-plant-details',
+	standalone: true,
+	imports: [ReactiveFormsModule, CommonModule, RouterLink],
+	template: `
+		<div>
+			Plant Details Component
+		</div>
+	`,
+	styles: ``,
 })
 export class PlantDetailsComponent {
+	plantForm: FormGroup = this.fb.group({
+		name: [
+			null,
+			Validators.compose([Validators.required, Validators.minLength(3)]),
+		],
+		type: [null, Validators.required],
+		status: [null, Validators.required],
+	});
 
+	constructor(
+		private fb: FormBuilder,
+		private route: ActivatedRoute,
+		private router: Router,
+		private plantService: PlantService
+	) {}
 }
